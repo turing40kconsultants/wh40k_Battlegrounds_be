4197a8e28509cdc2ab4eb7eb0d15d0442cfdf5bf
lance butler <lancej2004@gmail.com>
2024-07-31 16:41:08 -0600

    Feat: establishes route

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
+      resources :units, only: [:show]
       resources :factions, only: [:index] do
         resources :units, only: :index, action: :show, controller: '/api/v1/factions'
       end
