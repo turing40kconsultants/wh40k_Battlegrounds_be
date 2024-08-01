64ca194930107bfb3c0403ae4c9e2f01d2f4e6a7
Clyde <clyde.autin@gmail.com>
2024-08-01 13:57:10 -0700

    improves test cov to 97.08

--- app/controllers/api/v1/units_controller.rb
+++ app/controllers/api/v1/units_controller.rb
@@ -1,8 +1,8 @@
 class Api::V1::UnitsController < ApplicationController
-  def index
-    units = Unit.all
-    render json: UnitSerializer.format_units(units)
-  end
+  # def index
+  #   # units = Unit.all
+  #   # render json: UnitSerializer.format_units(units)
+  # end
 
   def show
     unit = Unit.find(params[:id])
